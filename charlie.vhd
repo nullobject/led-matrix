04483d69c600a72e720a2c95e15ef9caab52135b
Josh Bassett <josh.bassett@gmail.com>
2016-08-20 22:09:22 +1000

    PWM the LEDs.

--- charlie.vhd
+++ charlie.vhd
@@ -1,7 +1,6 @@
 library ieee;
 
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity charlie is
@@ -12,9 +11,10 @@ entity charlie is
 end charlie;
 
 architecture charlie_arch of charlie is
+  type pwm_type is array (0 to 7) of integer range 0 to 255;
   signal clock_enable : std_logic;
-  signal clock_enable_counter : std_logic_vector(23 downto 0);
-  signal count : std_logic_vector(3 downto 0);
+  signal clock_enable_counter : std_logic_vector(8 downto 0) := (others => '0');
+  signal pwm : pwm_type := (7, 15, 31, 63, 127, 255, 255, 255);
 begin
   clock_divider: process(clock)
   begin
@@ -30,20 +30,31 @@ begin
   end process;
 
   led_charlie: process(clock, clock_enable)
+    variable pwm_counter : integer range 0 to 255;
+    variable led : integer range 0 to 7;
+    variable value : std_logic_vector(3 downto 0);
   begin
     if rising_edge(clock) and clock_enable = '1' then
-      case count is
-        when "0001" => io <= "ZZ10"; -- 1
-        when "0010" => io <= "Z1Z0"; -- 2
-        when "0011" => io <= "1ZZ0"; -- 3
-        when "0100" => io <= "ZZ01"; -- 4
-        when "0101" => io <= "Z10Z"; -- 5
-        when "0110" => io <= "1Z0Z"; -- 6
-        when others => io <= "ZZZZ";
+      pwm_counter := pwm_counter + 1;
+
+      if pwm_counter = 0 then
+        led := led + 1;
+      end if;
+
+      case led is
+        when 0 => value := "ZZ10";
+        when 1 => value := "Z1Z0";
+        when 2 => value := "1ZZ0";
+        when 3 => value := "ZZ01";
+        when 4 => value := "Z10Z";
+        when 5 => value := "1Z0Z";
+        when others => value := "0000";
       end case;
-      count <= count + 1;
-      if count = "0111" then
-        count <= "0000";
+
+      if pwm_counter < pwm(led) then
+        io <= value;
+      else
+        io <= "ZZZZ";
       end if;
     end if;
   end process;
