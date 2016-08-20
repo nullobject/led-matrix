02b781bab2468e4d3f20ea9f1a94dfa014e8a790
Josh Bassett <josh.bassett@gmail.com>
2016-08-20 13:31:42 +1000

    Implement counter on IO (0:3).

--- charlie.vhd
+++ charlie.vhd
@@ -7,17 +7,33 @@ use ieee.std_logic_unsigned.all;
 entity charlie is
   port (
     clock : in std_logic;
-    leds  : out std_logic_vector(7 downto 0)
+    io    : out std_logic_vector(3 downto 0)
   );
 end charlie;
 
 architecture charlie_arch of charlie is
-  signal count : std_logic_vector(7 downto 0) := "11111111";
+  signal clock_enable : std_logic;
+  signal clock_enable_counter : std_logic_vector(23 downto 0);
+  signal count : std_logic_vector(3 downto 0) := "0000";
 begin
-  led_charlie: process(clock)
+  clock_divider: process(clock)
   begin
-    if clock'event and clock = '1' then
-      leds <= count;
+    if rising_edge(clock) then
+      clock_enable_counter <= clock_enable_counter + 1;
+
+      if clock_enable_counter = 0 then
+        clock_enable <= '1';
+      else
+        clock_enable <= '0';
+      end if;
+    end if;
+  end process;
+
+  led_charlie: process(clock, clock_enable)
+  begin
+    if rising_edge(clock) and clock_enable = '1' then
+      count <= count + 1;
+      io <= count;
     end if;
   end process;
 end charlie_arch;
