7a848b16f5a0e5f450a41099858679f97331c8ba
Josh Bassett <josh.bassett@gmail.com>
2016-08-20 15:11:14 +1000

    Chalieplex the pins.

--- charlie.vhd
+++ charlie.vhd
@@ -14,7 +14,7 @@ end charlie;
 architecture charlie_arch of charlie is
   signal clock_enable : std_logic;
   signal clock_enable_counter : std_logic_vector(23 downto 0);
-  signal count : std_logic_vector(3 downto 0) := "0000";
+  signal count : std_logic_vector(3 downto 0);
 begin
   clock_divider: process(clock)
   begin
@@ -32,8 +32,19 @@ begin
   led_charlie: process(clock, clock_enable)
   begin
     if rising_edge(clock) and clock_enable = '1' then
+      case count is
+        when "0001" => io <= "ZZ10"; -- 1
+        when "0010" => io <= "Z1Z0"; -- 2
+        when "0011" => io <= "1ZZ0"; -- 3
+        when "0100" => io <= "ZZ01"; -- 4
+        when "0101" => io <= "Z10Z"; -- 5
+        when "0110" => io <= "1Z0Z"; -- 6
+        when others => io <= "ZZZZ";
+      end case;
       count <= count + 1;
-      io <= count;
+      if count = "0111" then
+        count <= "0000";
+      end if;
     end if;
   end process;
 end charlie_arch;
