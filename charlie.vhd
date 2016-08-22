aec35d006b92f15a9c1c0b8c91f75c4daba407d4
Josh Bassett <josh.bassett@gmail.com>
2016-08-22 18:59:08 +1000

    WIP

--- charlie.vhd
+++ charlie.vhd
@@ -5,15 +5,16 @@ use ieee.std_logic_unsigned.all;
 
 entity charlie is
   port (
-    clock : in std_logic;
-    io    : out std_logic_vector(3 downto 0)
+    clock  : in std_logic;
+    row    : out std_logic_vector(3 downto 0);
+    column : out std_logic_vector(6 downto 0)
   );
 end charlie;
 
 architecture charlie_arch of charlie is
   type pwm_type is array (0 to 7) of integer range 0 to 255;
   signal clock_enable : std_logic;
-  signal clock_enable_counter : std_logic_vector(3 downto 0) := (others => '0');
+  signal clock_enable_counter : std_logic_vector(15 downto 0) := (others => '0');
   signal pwm : pwm_type := (7, 15, 31, 63, 127, 255, 255, 255);
 begin
   clock_divider: process(clock)
@@ -29,33 +30,28 @@ begin
     end if;
   end process;
 
-  pwm_charlie: process(clock)
+  row_scan: process(clock)
+    variable row_counter : std_logic_vector(3 downto 0) := "0001";
+  begin
+    if rising_edge(clock) and clock_enable = '1' then
+      row_counter := row_counter(2 downto 0) & row_counter(3);
+      row <= row_counter;
+    end if;
+  end process;
+
+  column_scan: process(clock)
     variable pwm_counter : integer range 0 to 255;
-    variable led : integer range 0 to 80;
-    variable value : std_logic_vector(3 downto 0);
   begin
     if rising_edge(clock) then
+      for i in 0 to 6 loop
+        if pwm_counter < pwm(i) then
+          column(i) <= '1';
+        else
+          column(i) <= '0';
+        end if;
+      end loop;
+
       pwm_counter := pwm_counter + 1;
-
-      if pwm_counter = 0 then
-        led := led + 1;
-      end if;
-
-      case led is
-        when 0 => value := "ZZ10";
-        when 1 => value := "Z1Z0";
-        when 2 => value := "1ZZ0";
-        when 3 => value := "ZZ01";
-        when 4 => value := "Z10Z";
-        when 5 => value := "1Z0Z";
-        when others => value := "0000";
-      end case;
-
-      if pwm_counter < pwm(led) then
-        io <= value;
-      else
-        io <= "ZZZZ";
-      end if;
     end if;
   end process;
 end charlie_arch;
