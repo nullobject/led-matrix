31be8c655bda13f5a72e3bfc97d891b522cba65c
Josh Bassett <josh.bassett@gmail.com>
2016-08-26 20:54:38 +1000

    Debounce SCL and SDA.

--- i2c_slave.vhd
+++ i2c_slave.vhd
@@ -2,10 +2,13 @@ library ieee;
 
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_misc.all;
 
 entity i2c_slave is
   generic (
-    SLAVE_ADDR : std_logic_vector(6 downto 0));
+    SLAVE_ADDR : std_logic_vector(6 downto 0);
+    DEBOUNCE_LENGTH : natural := 4
+  );
   port (
     scl              : inout std_logic;
     sda              : inout std_logic;
@@ -15,7 +18,8 @@ entity i2c_slave is
     read_req         : out   std_logic;
     data_to_master   : in    std_logic_vector(7 downto 0);
     data_valid       : out   std_logic;
-    data_from_master : out   std_logic_vector(7 downto 0));
+    data_from_master : out   std_logic_vector(7 downto 0)
+  );
 end entity i2c_slave;
 
 architecture arch of i2c_slave is
@@ -40,11 +44,16 @@ architecture arch of i2c_slave is
   signal data_reg             : std_logic_vector(6 downto 0) := (others => '0');
   signal data_from_master_reg : std_logic_vector(7 downto 0) := (others => '0');
 
-  signal scl_prev_reg : std_logic := '1';
+  signal scl_pipeline : std_logic_vector(DEBOUNCE_LENGTH-1 downto 0) := (others => '0');
+  signal sda_pipeline : std_logic_vector(DEBOUNCE_LENGTH-1 downto 0) := (others => '0');
+
+  signal scl_reg, scl_prev_reg : std_logic := '1';
+  signal sda_reg, sda_prev_reg : std_logic := '1';
+
   -- Slave writes on scl
   signal scl_wen_reg  : std_logic := '0';
   signal scl_o_reg    : std_logic := '0';
-  signal sda_prev_reg : std_logic := '1';
+
   -- Slave writes on sda
   signal sda_wen_reg  : std_logic := '0';
   signal sda_o_reg    : std_logic := '0';
@@ -57,36 +66,53 @@ begin
   process(clk) is
   begin
     if rising_edge(clk) then
+      scl_pipeline <= scl_pipeline(scl_pipeline'length-2 downto 0) & scl;
+
+      if or_reduce(scl_pipeline) = '0' then
+        scl_reg <= '0';
+      elsif and_reduce(scl_pipeline) = '1' then
+        scl_reg <= '1';
+      end if;
+
+      sda_pipeline <= sda_pipeline(sda_pipeline'length-2 downto 0) & sda;
+
+      if or_reduce(sda_pipeline) = '0' then
+        sda_reg <= '0';
+      elsif and_reduce(sda_pipeline) = '1' then
+        sda_reg <= '1';
+      end if;
+
       -- Delay SCL by 1 and 2 clock cycles
-      scl_prev_reg   <= scl;
+      scl_prev_reg <= scl_reg;
+
       -- Delay SDA by 1 and 2 clock cycles
-      sda_prev_reg   <= sda;
+      sda_prev_reg <= sda_reg;
+
       -- Detect rising and falling SCL
       scl_rising_reg <= '0';
-      if scl_prev_reg = '0' and scl = '1' then
+      if scl_prev_reg = '0' and scl_reg = '1' then
         scl_rising_reg <= '1';
       end if;
       scl_falling_reg <= '0';
-      if scl_prev_reg = '1' and scl = '0' then
+      if scl_prev_reg = '1' and scl_reg = '0' then
         scl_falling_reg <= '1';
       end if;
 
       -- Detect I2C START condition
       start_reg <= '0';
       stop_reg  <= '0';
-      if scl = '1' and scl_prev_reg = '1' and
-        sda_prev_reg = '1' and sda = '0' then
+      if scl_reg = '1' and scl_prev_reg = '1' and
+        sda_prev_reg = '1' and sda_reg = '0' then
         start_reg <= '1';
         stop_reg  <= '0';
       end if;
 
       -- Detect I2C STOP condition
-      if scl_prev_reg = '1' and scl = '1' and
-        sda_prev_reg = '0' and sda = '1' then
+      if scl_prev_reg = '1' and scl_reg = '1' and
+        sda_prev_reg = '0' and sda_reg = '1' then
         start_reg <= '0';
         stop_reg  <= '1';
       end if;
-
     end if;
   end process;
 
@@ -115,10 +141,10 @@ begin
           if scl_rising_reg = '1' then
             if bits_processed_reg < 7 then
               bits_processed_reg             <= bits_processed_reg + 1;
-              addr_reg(6-bits_processed_reg) <= sda;
+              addr_reg(6-bits_processed_reg) <= sda_reg;
             elsif bits_processed_reg = 7 then
               bits_processed_reg <= bits_processed_reg + 1;
-              cmd_reg            <= sda;
+              cmd_reg            <= sda_reg;
             end if;
           end if;
 
@@ -159,9 +185,9 @@ begin
           if scl_rising_reg = '1' then
             bits_processed_reg <= bits_processed_reg + 1;
             if bits_processed_reg < 7 then
-              data_reg(6-bits_processed_reg) <= sda;
+              data_reg(6-bits_processed_reg) <= sda_reg;
             else
-              data_from_master_reg <= data_reg & sda;
+              data_from_master_reg <= data_reg & sda_reg;
               data_valid_reg       <= '1';
             end if;
           end if;
