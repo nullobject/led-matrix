75f5c7588ac7973c15eccd4a534df2845b0a39f7
Josh Bassett <josh.bassett@gmail.com>
2017-01-26 22:26:51 +1100

    Increment address for sequential writes

--- top.vhd
+++ top.vhd
@@ -1,6 +1,8 @@
 library ieee;
 
 use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 use work.automata.all;
 
@@ -24,12 +26,12 @@ end charlie;
 
 architecture arch of charlie is
   signal ram_we     : std_logic;
-  signal ram_addr_a : std_logic_vector(ADDR_WIDTH-1 downto 0);
+  signal ram_addr_a, next_ram_addr : std_logic_vector(ADDR_WIDTH-1 downto 0);
   signal ram_din_a  : std_logic_vector(DATA_WIDTH-1 downto 0);
   signal ram_addr_b : std_logic_vector(ADDR_WIDTH-1 downto 0);
   signal ram_dout_b : std_logic_vector(DATA_WIDTH-1 downto 0);
 
-  type state_t is (idle_state, data_state);
+  type state_t is (addr_state, data_state, inc_state);
   signal state : state_t;
 
   signal spi_rx_data : std_logic_vector(DATA_WIDTH-1 downto 0);
@@ -57,8 +59,8 @@ begin
       clk    => clk50,
       we     => ram_we,
       addr_a => ram_addr_a,
-      addr_b => ram_addr_b,
       din_a  => ram_din_a,
+      addr_b => ram_addr_b,
       dout_b => ram_dout_b
     );
 
@@ -87,24 +89,34 @@ begin
   spi_handler : process(rst, clk50, spi_done)
   begin
     if rst = '1' then
-      state <= idle_state;
+      state <= addr_state;
     elsif rising_edge(clk50) then
       ram_we <= '0';
 
-      case state is
-      when idle_state =>
-        if spi_done = '1' then
-          ram_we <= '1';
-          ram_addr_a <= spi_rx_data(ADDR_WIDTH-1 downto 0);
+      if ss = '1' then
+        state <= addr_state;
+      else
+        case state is
+        when addr_state =>
+          if spi_done = '1' then
+            ram_addr_a <= spi_rx_data(ADDR_WIDTH-1 downto 0);
+            state <= data_state;
+          end if;
+
+        when data_state =>
+          if spi_done = '1' then
+            ram_we <= '1';
+            next_ram_addr <= ram_addr_a + 1;
+            ram_din_a <= spi_rx_data;
+            state <= inc_state;
+          end if;
+
+        when inc_state =>
+          ram_addr_a <= next_ram_addr;
           state <= data_state;
-        end if;
-      when data_state =>
-        if spi_done = '1' then
-          ram_we <= '1';
-          ram_din_a <= spi_rx_data;
-          state <= idle_state;
-        end if;
-      end case;
+
+        end case;
+      end if;
     end if;
   end process;
 
