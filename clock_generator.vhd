5929327669990e86e8e57d2febdf13e6b1e12b0a
Josh Bassett <josh.bassett@gmail.com>
2017-04-28 15:53:19 +1000

    Clock generator cleanups

--- clock_generator.vhd
+++ clock_generator.vhd
@@ -1,51 +1,53 @@
 library ieee;
+
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 library unisim;
+
 use unisim.vcomponents.all;
 
 entity clock_generator is
   port (
-    clkin_in        : in  std_logic;
-    rst_in          : in  std_logic;
-    clkfx_out       : out std_logic;
-    clkin_ibufg_out : out std_logic;
-    clk0_out        : out std_logic;
-    locked_out      : out std_logic
+    clkin_in:        in  std_logic;
+    rst_in:          in  std_logic;
+    clkfx_out:       out std_logic;
+    clkin_ibufg_out: out std_logic;
+    clk0_out:        out std_logic;
+    locked_out:      out std_logic
   );
 end clock_generator;
 
 architecture arch of clock_generator is
-  signal clkfb_in    : std_logic;
-  signal clkfx_buf   : std_logic;
-  signal clkin_ibufg : std_logic;
-  signal clk0_buf    : std_logic;
-  signal gnd_bit     : std_logic;
+  signal clkfb_in:    std_logic;
+  signal clkfx_buf:   std_logic;
+  signal clkin_ibufg: std_logic;
+  signal clk0_buf:    std_logic;
+  signal gnd_bit:     std_logic;
 begin
   gnd_bit <= '0';
   clkin_ibufg_out <= clkin_ibufg;
   clk0_out <= clkfb_in;
 
-  clkfx_bufg_inst : bufg
+  clkfx_bufg_inst: bufg
     port map (
       i => clkfx_buf,
       o => clkfx_out
     );
 
-  clkin_ibufg_inst : ibufg
+  clkin_ibufg_inst: ibufg
     port map (
       i => clkin_in,
       o => clkin_ibufg
     );
 
-  clk0_bufg_inst : bufg
+  clk0_bufg_inst: bufg
     port map (
       i => clk0_buf,
       o => clkfb_in
     );
 
-  dcm_sp_inst : dcm_sp
+  dcm_sp_inst: dcm_sp
     generic map (
       clk_feedback          => "1X",
       clkdv_divide          => 2.0,
